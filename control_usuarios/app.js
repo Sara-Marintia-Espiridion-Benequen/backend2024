7237c583ae03714626bb366c13ca6160262135cb
Sara Marintia Espiridion Benequen <sarah93.ad@gmail.com>
2024-10-07 12:03:00 -0600

    Agrega el proyecto control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 
 const app = express();
+const usuarios = [
+    {
+        id: 1,
+        nombre: "Sara",
+        apellido:"Espiridion",
+        email:"sarah93.ad@gmail.com",
+    },
+    {
+        id: 2,
+        nombre: "Juan",
+        apellido:"Teodoro",
+        email:"JuanTeodor.ad@gmail.com",
+    },
+]
 
 app.get("/usuarios", (req, res) =>{
-    const usuarios = [
-        {
-            id: 1,
-            nombre: "Sara",
-            apellido:"Espiridion",
-            email:"sarah93.ad@gmail.com",
-        },
-    ]
-    res.status(404).send(usuarios);
+    res.status(200).send(usuarios);
+})
+
+app.get("/usuarios/:id", (req, res) => {
+    const {id} = req.params;
+    const usuario = usuarios .find((usuario) => usuario.id === +id);
+
+    res.status(200).send(usuario);
 })
 
 app.listen(3000, () => {
