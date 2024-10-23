38e62ec702b12ea913a3ad28d2e61c6a584ffcd5
Sara Marintia Espiridion Benequen <sarah93.ad@gmail.com>
2024-10-23 17:50:24 -0600

    Agregue el proyecto stock_manager

---
+++ stock_manager/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = 3000;
+        this.app.use(express.json);
+    }
+
+    start() {
+        this.app.listen(this.port, () =>{
+            console.log('Server listening on port' + this.port);
+         });
+    }
+}
+
+module.exports = {Server};
