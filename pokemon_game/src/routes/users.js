3f64d983647d5e8dc06fd23fd21ca4f95624a089
Sara Marintia Espiridion Benequen <sarah93.ad@gmail.com>
2024-12-02 12:45:50 -0600

    pokemon_game 02/12/2024

---
+++ pokemon_game/src/routes/users.js
@@ -0,0 +1,11 @@
+const {Router} = require('express');
+const {getAllUsers} = require ('../controllers/users');
+
+const router = Router();
+router.get('/', getAllUsers);
+router.get('/:id');
+router.post('/');
+router.put('/:id');
+router.delete('/:id');
+
+module.exports = router;
