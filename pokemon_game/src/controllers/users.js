fffd375dd08889a8f2a4356208c358edbe3d49bf
Sara Marintia Espiridion Benequen <sarah93.ad@gmail.com>
2024-12-12 11:20:28 -0600

    pokemon_game 12/12/2024

--- pokemon_game/src/controllers/users.js
+++ pokemon_game/src/controllers/users.js
@@ -1,171 +1,195 @@
-const {request, response} = require ('express');
+const {request, response} = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const pool = require('../db/connection');
-const userQueries = require('../models/users');
+const {userQueries} = require('../models/users');
+require('dotenv').config();
 
-const SALT_ROUNDS = 10;
+const secret = process.env.SECRET;
+
+const saltRounds = 10;
 
 
-const getAllUsers = async (req = request, res = response) => {
+const userProtected = async(req = request, res = response) =>{
+  res.send({message: "you have access!!"});
+}
+
+
+const getAllUsers = async (req = request, res=response) => {
     let conn;
 
     try{
-        conn = await pool.getConnection();
+        conn=await pool.getConnection();
         const users = await conn.query(userQueries.getAll);
 
         res.json(users);
         return;
-    } catch (err) {
+    }catch(err){
         res.status(500).json(err);
         return;
-    } finally {
-        if (conn) conn.end();
+
+    }finally{
+        if(conn) conn.end()
+
     }
 };
-
-const createUser = async (req = request, res = response) => {
-    const {first_name,
-        last_name,
-        email,
-        password
-    } = req.body;
-
-    if(!first_name || !last_name || !email || !password) {
-        res.status(400).json({message: 'Missing required fields'});
-        return;
-    }
-
-    let conn;
-    try{
-        conn = await pool.getConnection();
-        const [user_exist] = await conn.query(userQueries.getByEmail, [email]);
-
-        if (user_exist) {
-            res.status(400).send({message: 'User already exists'});
-            return;
-        }
-
-        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-
-        const newUser = await conn.query(userQueries.addUser, [first_name, last_name, email, hashedPassword]);
-
-        if(newUser.affetRows === 0) {
-            res.status(500).send({message: 'Error adding user'});
-            return;
-        }
-
-        res.status(201).send({message: 'User created'});
-    } catch (err) {
-        res.status(500).json(err);
-        return;
-    } finally {
-        if (conn) conn.end;
-    }
-};
-
-const getUser = async (req = request, res = response) => {
+const getUserById = async (req = request, res= response) =>{
     const {id} = req.params;
-
-    if (isNaN(id)) {
-        res.status(400).send({message: 'Invalid user ID'});
+    if(isNaN(id)){
+        res.status(400).send('Invalid ID');
+        return;
     }
-
     let conn;
     try{
         conn = await pool.getConnection();
-        const user = await conn.query(userQueries.getById, [+id]);
-
-        if(!user) {
-            res.status(404).send({message: 'User not found'});
+        const [user] = await conn.query(userQueries.getById, [+id]);
+        if(!user){
+            res.status(404).send('User not found')
+            return;
         }
-    } catch{
-        res.status(500).send(err);
-        return;
-    } finally {
-        if (conn) conn.end;
+    
+    res.send(user);
+    }catch (error){
+        res.status(500).send(error);
+    }finally{
+        if(conn) conn.end();
     }
+
+    //const user = users.find(user => user.id === +id);
+    
 }
+//agregar un usuario
+const CreateUser = async (req = request, res = response) => {
+    const { first_name, last_name, email, password} = req.body;
 
+    if (!first_name || !last_name || !email || !password) {
+        res.status(400).send('Bad Request. Some fields are missing');
+        return;
+    }
+
+    let conn;
+    try{
+        conn = await pool.getConnection();
+        const [user_exits] = await conn.query(userQueries.getByEmail, [email]);
+        if(user_exits){
+            res.status(409).send('Email already exits');
+            return;
+        }
+
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+
+        const newUser = await conn.query(userQueries.create, [first_name, last_name, email, hashPassword]);
+        if(newUser.affectedRows ===0){
+            res.status(500).send('Error adding user');
+            return;
+        }
+        res.status(201).send("user created succefully");
+    }catch (error){
+        res.status(500).send(error);
+        return;
+    }finally{
+        if(conn) conn.end();
+    }
+
+}
 const updateUser = async (req = request, res = response) => {
-    const {id} = req.params;
-    const {
+    const { id } = req.params;
+    const { first_name, last_name, email } = req.body;
+  
+    if (isNaN(id)) {
+      res.status(400).send("Invalid ID");
+      return;
+    }
+  
+    if (!first_name || !last_name || !email) {
+      res.status(400).send("At least one field is required for update");
+      return;
+    }
+  
+    let conn;
+  
+    try {
+      conn = await pool.getConnection();
+  
+      // Verificar si el usuario existe
+      const [user] = await conn.query(userQueries.getById, [+id]);
+      if (!user) {
+        res.status(404).send("User not found");
+        return;
+      }
+  
+      // Verificar si el correo electrónico ya está en uso por otro usuario
+      const [emailExist] = await conn.query(userQueries.emailvalid, [email, id]);
+      if (emailExist) {
+        res.status(409).send({ message: 'Email already in use' });
+        return;
+      }
+  
+      // Actualizar el usuario
+      const { affectedRows } = await conn.query(userQueries.editUser, [
         first_name,
         last_name,
-        email
-    } = req.body;
-
-    if (isNaN(id)) {
-        res.status(400).send({message: 'Ivalid user ID'});
+        email,
+        id,
+      ]);
+  
+      if (affectedRows === 0) {
+        res.status(500).send({ message: 'User not updated' });
         return;
-    }
-
-    if (!first_name || !last_name || !email) {
-        res.status
-    }
-
-    try{
-        conn = await pool.getConnection();
-        const [user] = await conn.query(userQueries.getById, [id]);
-        if(!user) {
-            res.status(404).send({message: 'User not found'});
-            return;
-        }
-
-        const [emailExists] = await conn.query(userQueries.emailValid, [email, id]);
-        if (emailExists) {
-            res.status(409).send({message: 'Email already in use'});
-            return;
-        }
-
-        const updateUser = await conn.query(userQueries.editUser, [first_name, last_name, email, id]);
-        if (updateUser.affetRows === 0) {
-            res.status(500).send({message: 'User not updated'});
-            return;
-        }
-        res.send({message: 'User updates'});
-     } catch (err) {
-        res.status(500).json(err);
+      }
+  
+      res.send("User updated successfully");
+    } catch (error) {
+      res.status(500).send(error);
+      return;
     } finally {
-        if (conn) conn.end();
+      if (conn) conn.end();
     }
-}
-
-const destroyUser = async (req = request, res = response) => {
-const {id} = req.params;
-
-if (isNaN(id)) {
-    res.status(404).send({message: 'Invalid user ID'});
-    return;
-}
-
+  };
+  const destroyUser = async (req = request, res = response) => {
+    const { id } = req.params;
+  
+    if (isNaN(id)) {
+      res.status(400).send({ message: 'Invalid ID' });
+      return;
+    }
+  
     let conn;
+  
     try {
-        conn = await pool.getConnection();
-        const [user] = await conn.query(userQueries.getById, [id]);
-        
-        if (!user) {
-            res.status(404).send({message: 'User not found'});
-            return;
-        }
-        const deleteUser = await conn.query(userQueries.deleteUser, [id]);
-        if (deleteUser.affetRows === 0) {
-            res.status(500).send({message: 'Error deleting user'});
-            return;
-        }
-
-        res.status({message:'User deleted'});
-    } catch (err){
-        res.status(500).send(err);
+      conn = await pool.getConnection();
+  
+      // Verificar si el usuario existe
+      const [user] = await conn.query(userQueries.getById, [+id]);
+      if (!user) {
+        res.status(404).send({ message: 'User not found' });
+        return;
+      }
+  
+      // Eliminar el usuario
+      const { affectedRows } = await conn.query(userQueries.deleteUser, [id]);
+  
+      if (affectedRows === 0) {
+        res.status(500).send({ message: 'Error deleting user' });
+        return;
+      }
+  
+      res.send({ message: 'User deleted successfully' });
+    } catch (error) {
+      res.status(500).send(error);
+      return;
     } finally {
-        if (conn) conn.end();
+      if (conn) conn.end();
     }
-}
+  };
+
 
 
 module.exports = {
     getAllUsers,
-    createUser,
-    getUser,
+    getUserById,
+    CreateUser,
     updateUser,
     destroyUser,
+    userProtected
 };
--- pokemon_game/src/routes/users.js
+++ pokemon_game/src/routes/users.js
@@ -1,11 +1,14 @@
-const {Router} = require('express');
-const {getAllUsers, createUser, getUser, updateUser, destroyUser} = require ('../controllers/users');
+const { Router } = require('express');
+const { getAllUsers, getUserById, CreateUser, updateUser, destroyUser, userProtected} = require('../controllers/users');
+const verifyToken = require('../middlewares/verifyToken');
 
 const router = Router();
 router.get('/', getAllUsers);
-router.get('/:id', getUser);
-router.post('/', createUser);
-router.put('/:id', updateUser);
+router.get('/protected', verifyToken, userProtected)
+router.get('/:id',getUserById);
+router.post('/', CreateUser);
+router.put('/:id',updateUser);
 router.delete('/:id', destroyUser);
+
 
 module.exports = router;
